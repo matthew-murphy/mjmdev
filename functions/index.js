984e7f8d87af9b40cbf2fb32b39ab28bc44075e1
Matthew Murphy <matthewmurphy@Matthews-MacBook-Pro.local>
2023-01-26 22:12:08 -0500

    website update 2.0

--- functions/index.js
+++ functions/index.js
@@ -1,48 +1,15 @@
-const functions = require('firebase-functions')
-const nodemailer = require('nodemailer')
-const cors = require('cors')({
-  origin: true
-})
-const gmailEmail = functions.config().gmail.email
-const gmailPassword = functions.config().gmail.password
+'use strict';
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: gmailEmail,
-    pass: gmailPassword,
-  },
-})
+app.use(cors({ origin: true }));
 
-exports.submit = functions.https.onRequest((req, res) => {
-  res.set('Access-Control-Allow-Origin', '*')
-  res.set('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS')
-  res.set('Access-Control-Allow-Headers', '*')
+// Init Middleware
+app.use(express.json());
 
-  if (req.method === 'OPTIONS') {
-    res.end()
-  } else {
-    cors(req, res, () => {
-      if (req.method !== 'POST') {
-        return
-      }
+// Define routes
+app.use('/api/contacts', require('./api/contacts'));
 
-      const mailOptions = {
-        from: req.body.email,
-        replyTo: req.body.email,
-        to: gmailEmail,
-        subject: `${req.body.name} just messaged me from my website`,
-        text: req.body.message,
-        html: `<p>${req.body.message}</p>`,
-      }
-
-      return mailTransport.sendMail(mailOptions).then(() => {
-        console.log('New email sent to:', gmailEmail)
-        res.status(200).send({
-          isEmailSend: true
-        })
-        return
-      })
-    })
-  }
-})
+exports.app = functions.https.onRequest(app);
--- src/components/Cards/PorfolioCard.js
+++ src/components/Cards/PorfolioCard.js
@@ -1,14 +1,7 @@
-import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
 import {
   Button,
   Card
 } from "reactstrap";
-// import {
-//   CardActions,
-//   CardContent,
-//   Typography,
-// } from "@material-ui/core";
 import { CardContent, Typography, CardActions } from '@mui/material';
 
 //props: name, src, description, site, repo
@@ -21,38 +14,39 @@ export default function PortfolioCard(props) {
   // const classes = useStyles();
 
   return (
-    <Card variant="elevation">
+    <Card variant="elevation" style={{marginTop: '0px'}}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="h2">
           {props.name}
         </Typography>
 
-        <img src={props.src} classes="icon-1" alt="html5-icon" />
-        <Typography variant="body2" color="textSecondary" component="p">
+        <img src={props.src} alt="html5-icon" className="img-stretch" />
+        <Typography variant="body2" color="textSecondary" component="p" style={{paddingTop: '24px'}}>
           {props.description}
           {props.techs}
         </Typography>
       </CardContent>
-      <CardActions>
+      <CardActions style={{paddingLeft: '16px'}}>
         <Button
           onClick={(e) => openInNewTab(props.site)}
-          // href={props.site}
           variant="outlined"
           size="small"
           color="secondary"
         >
           Site
         </Button>
-
-        <Button
-          onClick={(e) => openInNewTab(props.repo)}
-          // href={props.repo}
-          variant="outlined"
-          size="small"
-          color="secondary"
-        >
-          Repo
-        </Button>
+        {props.repo ?
+          <Button
+            onClick={(e) => openInNewTab(props.repo)}
+            href={props.repo}
+            variant="outlined"
+            size="small"
+            color="secondary"
+          >
+            Repo
+          </Button>
+         : null
+        }
       </CardActions>
     </Card>
   );

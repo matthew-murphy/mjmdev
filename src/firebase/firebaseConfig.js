af63fccf6c0a8deded2cc2d812f0ef21b7e494d2
Matthew Murphy <matthewmurphy@Matthews-MacBook-Pro.local>
2023-01-26 22:12:08 -0500

    website update 2.0

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,6 +2,8 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/database'
+import 'firebase/compat/storage'
+import 'firebase/compat/app-check'
 import Axios from 'axios'
 
 // Initialize Firebase
@@ -14,8 +16,24 @@ let config = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 }
 
-firebase.initializeApp(config)
+const app = firebase.initializeApp(config)
 
 const db = firebase.firestore()
 
-export { Axios, db }
+// initializeAppCheck(app, {
+//   provider: new ReCaptchaV3Provider(process.env.REACT_APP_SITE_KEY),
+//   isTokenAutoRefreshEnabled: true,
+// });
+
+
+const appCheck = firebase.appCheck();
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+appCheck.activate(
+  process.env.REACT_APP_SITE_KEY,
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  false);
+
+export { Axios, db, app }
